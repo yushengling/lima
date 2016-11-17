6c18215badefcb3e98374f4c85ab405c08a022ce
yushengling <1121513947@qq.com>
2016-11-17 09:45:02 +0800

    第一次提交

---
+++ app/src/main/java/com/ling/lima/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ling.lima;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        你好
+//        试一下水
+    }
+}
